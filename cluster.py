df626db50e4224ed043b76d0a5b76719a32872a9
CoolHandN <36666301+CoolhandinMotion@users.noreply.github.com>
2024-03-22 15:42:27 +0100

    removed coupling among methods and such. should be more readable now

--- cluster.py
+++ cluster.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from dataclasses import dataclass, field
 from numpy.typing import NDArray
 from typing import List, Tuple, Dict
 import numpy as np
@@ -9,9 +8,25 @@ k_array = np.asarray([10,10,1])
 epsilon = 10**-6
 cluster_2_gird_dict = Dict[int, List[Tuple[int, int]]]
 grid_2_data_dict = Dict[Tuple[int, int], NDArray]
+
+
+
+class Cluster:
+    clusters = defaultdict()
+    def __init__(self, index: int, members_grid: List[Tuple[int, int]]):
+        self.index = index
+        self.members_grid = members_grid
+        self.clusters[index] = self
+        self.data = None
+        self.mean = None
+        self.gravity = None
+        self.inv_cov_matrix = None
+        self.std = None
+    def remove(self):
+        self.clusters.pop(self.index)
 def initiate_clustering(cluster_2_grid: cluster_2_gird_dict) -> None:
     for index in cluster_2_grid.keys():
-         c = Cluster(index = index, members_grid= cluster_2_grid[index])
+         Cluster(index = index, members_grid= cluster_2_grid[index])
 def get_cluster_data_from_grid(grid_2_data: Dict[Tuple[int, int], NDArray],
                                members_grid:List[Tuple[int, int]]) -> NDArray:
     try:
@@ -30,19 +45,16 @@ def update_cluster(cluster_index: int,grid_2_data: grid_2_data_dict, k_adjusment
     if len(cluster.members_grid)<=1:
         cluster.remove()
         return
-    update_cluster_data(grid_2_data,cluster_index)
-    update_cluster_mean(cluster_index)
-    update_cluster_std(cluster_index)
-    update_cluster_gravity(cluster_index,k_adjusments)
+    update_cluster_data(grid_2_data,cluster)
+    update_cluster_mean(cluster)
+    update_cluster_std(cluster)
+    update_cluster_gravity(cluster,k_adjusments)
 
-def update_cluster_data(grid_2_data: grid_2_data_dict,cluster_index: int) -> None:
-    cluster = Cluster.clusters[cluster_index]
+def update_cluster_data(grid_2_data: grid_2_data_dict,cluster: Cluster) -> None:
     cluster.data = get_cluster_data_from_grid(grid_2_data,cluster.members_grid)
-def update_cluster_mean(cluster_index: int) -> None:
-    cluster = Cluster.clusters[cluster_index]
+def update_cluster_mean(cluster: Cluster) -> None:
     cluster.mean = np.mean(cluster.data,axis=0)
-def update_cluster_std(cluster_index: int) ->None:
-    cluster = Cluster.clusters[cluster_index]
+def update_cluster_std(cluster: Cluster) ->None:
     std = np.std(cluster.data,axis=0)
     # below should be a one liner lambda function
     if std.any() == 0:
@@ -51,8 +63,7 @@ def update_cluster_std(cluster_index: int) ->None:
                 std[i] = epsilon
     cluster.std = std
 
-def update_cluster_gravity(cluster_index: int, k_adjusments: NDArray) ->None:
-    cluster = Cluster.clusters[cluster_index]
+def update_cluster_gravity(cluster: Cluster, k_adjusments: NDArray) ->None:
     k_times_sigma = cluster.std * k_adjusments
     adjusted_force_array = (np.power(k_times_sigma+1,-1)*k_adjusments)**2
     force = np.sum(adjusted_force_array)
@@ -65,20 +76,8 @@ def update_cluster_gravity(cluster_index: int, k_adjusments: NDArray) ->None:
 
 
 
-@dataclass
-class Cluster:
-    clusters = defaultdict()
-    def __init__(self, index: int, members_grid: List[Tuple[int, int]]):
-        self.index = index
-        self.members_grid = members_grid
-        self.clusters[index] = self
-        self.data = None
-        self.mean = None
-        self.gravity = None
-        self.inv_cov_matrix = None
-        self.std = None
-    def remove(self):
-        self.clusters.pop(self.index)
+
+
 
 
 
