884d220a925c993bafbcbfdd2145e8490514e876
CoolHandN <36666301+CoolhandinMotion@users.noreply.github.com>
2024-03-22 15:13:50 +0100

    clustering based on OOP works.

--- cluster.py
+++ cluster.py
@@ -1,10 +1,74 @@
-from dataclasses import dataclass,field
+from collections import defaultdict
+from dataclasses import dataclass, field
+from numpy.typing import NDArray
+from typing import List, Tuple, Dict
+import numpy as np
+
+gravity_constant_3d = np.sqrt(2*np.pi**3)
+k_array = np.asarray([10,10,1])
+epsilon = 10**-6
+cluster_2_gird_dict = Dict[int, List[Tuple[int, int]]]
+grid_2_data_dict = Dict[Tuple[int, int], NDArray]
+def initiate_clustering(cluster_2_grid: cluster_2_gird_dict) -> None:
+    for index in cluster_2_grid.keys():
+         c = Cluster(index = index, members_grid= cluster_2_grid[index])
+def get_cluster_data_from_grid(grid_2_data: Dict[Tuple[int, int], NDArray],
+                               members_grid:List[Tuple[int, int]]) -> NDArray:
+
+    cluster_arr = np.vstack([grid_2_data[grid] for grid in members_grid])
+    return cluster_arr
+
+def update_cluster(cluster_index: int,grid_2_data: grid_2_data_dict, k_adjusments: NDArray)->None:
+    update_cluster_data(grid_2_data,cluster_index)
+    update_cluster_mean(cluster_index)
+    update_cluster_std(cluster_index)
+    update_cluster_gravity(cluster_index,k_adjusments)
+
+def update_cluster_data(grid_2_data: grid_2_data_dict,cluster_index: int) -> None:
+    cluster = Cluster.clusters[cluster_index]
+    cluster.data = get_cluster_data_from_grid(grid_2_data,cluster.members_grid)
+def update_cluster_mean(cluster_index: int) -> None:
+    cluster = Cluster.clusters[cluster_index]
+    cluster.mean = np.mean(cluster.data,axis=0)
+def update_cluster_std(cluster_index: int) ->None:
+    cluster = Cluster.clusters[cluster_index]
+    std = np.std(cluster.data,axis=0)
+    # below should be a one liner lambda function
+    if std.any() == 0:
+        for i in range(3):
+            if std[i] ==0:
+                std[i] = epsilon
+    cluster.std = std
+
+def update_cluster_gravity(cluster_index: int, k_adjusments: NDArray) ->None:
+    cluster = Cluster.clusters[cluster_index]
+    k_times_sigma = cluster.std * k_adjusments
+    adjusted_force_array = (np.power(k_times_sigma+1,-1)*k_adjusments)**2
+    force = np.sum(adjusted_force_array)
+    det = np.product(k_times_sigma)
+    cluster.gravity = gravity_constant_3d*det*force*np.exp(force/2)
+
+
+
+
+
+
 
 @dataclass
 class Cluster:
-    id: int
-    mean: float
-    gravity: float
-    std: float
-    cov_matrix: float
-    members: list
+    clusters = defaultdict()
+    def __init__(self, index: int, members_grid: List[Tuple[int, int]]):
+        self.index = index
+        self.members_grid = members_grid
+        self.clusters[index] = self
+        self.data = None
+        self.mean = None
+        self.gravity = None
+        self.inv_cov_matrix = None
+        self.std = None
+
+
+
+
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,45 @@
 from collections import defaultdict
 import numpy as np
 from numpy.typing import NDArray
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from PIL import Image
 
+image_2_data_dict: Dict[Tuple[int, int], NDArray] = {}
 
 
-def grayscale_tile(image: NDArray, tile_size: int) -> Dict[int, List[NDArray]]:
-    cluster_2_data_dict = defaultdict()
-    assert len(image.shape) == 2
-    assert isinstance(tile_size,int)
-    n_rows = image.shape[0]
-    n_cols = image.shape[1]
+#  by grid always a tuple(int, int) is meant. This denotes to grid that holds the
+#  gray scale image so image[int][int] would be accessible using the tuple key.
+# throughout the whole code the key that refers to grids in image is a tuple(row: int, column: int)
+
+# by data here the array is meant that holds the value [row, column, gray_scale_value]
+
+def grid_tiling(grid_shape: tuple, tile_size: int) -> Tuple[Dict[int, List[Tuple[int, int]]],
+                                                            Dict[Tuple[int, int], int]]:
+    cluster_2_grid_dict = defaultdict()
+    grid_2_cluster_dict = defaultdict()
+    assert len(grid_shape) == 2
+    assert isinstance(tile_size, int)
+    n_rows = grid_shape[0]
+    n_cols = grid_shape[1]
     first_iter_over_rows = int(n_rows // tile_size)
     first_iter_over_cols = int(n_cols // tile_size)
 
     cluster_index = 1
     for i in range(first_iter_over_rows):
         for j in range(first_iter_over_cols):
-            cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]]) for t in
+            cluster_2_grid_dict[cluster_index] = [(t, k) for t in
                                                   range(i * tile_size, (i + 1) * tile_size)
                                                   for k in
                                                   range(j * tile_size, (j + 1) * tile_size)]
+            temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                        for t in range(i * tile_size, (i + 1) * tile_size)
+                                        for k in range(j * tile_size, (j + 1) * tile_size)}
+            grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
             cluster_index += 1
 
     # if 2d Data shape is totally divisible by tile_size we are done here
     if n_rows % tile_size == 0 and n_cols % tile_size == 0:
-        return cluster_2_data_dict
+        return cluster_2_grid_dict, grid_2_cluster_dict
 
     # if the 2d Data shape divided by tile_size produces some leftover we need to cover that as well
 
@@ -38,23 +51,35 @@ def grayscale_tile(image: NDArray, tile_size: int) -> Dict[int, List[NDArray]]:
         if ideal_col_length < first_col_index_not_covered:
             last_iter_over_cols = first_col_index_not_covered // ideal_col_length
             for i in range(last_iter_over_cols):
-                cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
-                                                      for t in
-                                                      range(first_row_index_not_covered, n_rows)
-                                                      for k in range(ideal_col_length * i,
-                                                                     ideal_col_length * (i + 1))]
+                cluster_2_grid_dict[cluster_index] = [(t, k)
+                                                      for t in range(first_row_index_not_covered, n_rows)
+                                                      for k in range(ideal_col_length * i, ideal_col_length * (i + 1))]
+
+                temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                            for t in range(first_row_index_not_covered, n_rows)
+                                            for k in range(ideal_col_length * i, ideal_col_length * (i + 1))}
+                grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
+
                 cluster_index += 1
 
-            cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
+            cluster_2_grid_dict[cluster_index] = [(t, k)
                                                   for t in range(first_row_index_not_covered, n_rows)
-                                                  for k in
-                                                  range(last_iter_over_cols * ideal_col_length,
-                                                        first_col_index_not_covered)]
+                                                  for k in range(last_iter_over_cols * ideal_col_length,
+                                                                 first_col_index_not_covered)]
+            temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                        for t in range(first_row_index_not_covered, n_rows)
+                                        for k in range(last_iter_over_cols * ideal_col_length,
+                                                       first_col_index_not_covered)}
+            grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
             cluster_index += 1
         else:
-            cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
+            cluster_2_grid_dict[cluster_index] = [(t, k)
                                                   for t in range(first_row_index_not_covered, n_rows)
                                                   for k in range(first_col_index_not_covered)]
+            temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                        for t in range(first_row_index_not_covered, n_rows)
+                                        for k in range(first_col_index_not_covered)}
+            grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
             cluster_index += 1
 
     if n_cols % tile_size != 0:
@@ -63,42 +88,63 @@ def grayscale_tile(image: NDArray, tile_size: int) -> Dict[int, List[NDArray]]:
         if ideal_row_length < first_row_index_not_covered:
             last_iter_over_rows = first_row_index_not_covered // ideal_row_length
             for i in range(last_iter_over_rows):
-                cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
+                cluster_2_grid_dict[cluster_index] = [(t, k)
                                                       for t in range(ideal_row_length * i,
                                                                      ideal_row_length * (i + 1))
                                                       for k in
                                                       range(first_col_index_not_covered, n_cols)]
+                temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                            for t in range(ideal_row_length * i,
+                                                           ideal_row_length * (i + 1))
+                                            for k in
+                                            range(first_col_index_not_covered, n_cols)}
+                grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
                 cluster_index += 1
 
-            cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
+            cluster_2_grid_dict[cluster_index] = [(t, k)
                                                   for t in
                                                   range(ideal_row_length * last_iter_over_rows,
                                                         first_row_index_not_covered)
                                                   for k in range(first_col_index_not_covered, n_cols)]
+            temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                        for t in
+                                        range(ideal_row_length * last_iter_over_rows,
+                                              first_row_index_not_covered)
+                                        for k in range(first_col_index_not_covered, n_cols)}
+            grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
             cluster_index += 1
 
         else:
-            cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
+            cluster_2_grid_dict[cluster_index] = [(t, k)
                                                   for t in range(first_row_index_not_covered)
                                                   for k in range(first_col_index_not_covered, n_cols)]
+            temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                        for t in range(first_row_index_not_covered)
+                                        for k in range(first_col_index_not_covered, n_cols)}
+            grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
             cluster_index += 1
 
     if n_rows % tile_size != 0 and n_cols % tile_size != 0:
-        cluster_2_data_dict[cluster_index] = [np.asarray([t, k, image[t][k]])
+        cluster_2_grid_dict[cluster_index] = [(t, k)
                                               for t in range(first_row_index_not_covered, n_rows)
                                               for k in range(first_col_index_not_covered, n_cols)]
-    return cluster_2_data_dict
+        temp_grid_2_cluster_dict = {(t, k): cluster_index
+                                    for t in range(first_row_index_not_covered, n_rows)
+                                    for k in range(first_col_index_not_covered, n_cols)}
+        grid_2_cluster_dict.update(temp_grid_2_cluster_dict)
+    return cluster_2_grid_dict, grid_2_cluster_dict
 
 
-def load_image(image_url: str, standard_scaling = False, round_decimal: int = 6) -> NDArray:
+def load_image(image_url: str, standard_scaling=False, round_decimal: int = 6) -> NDArray:
     image = Image.open(image_url)
     image_array = np.asarray(image)
     if standard_scaling:
-        image_array = np.around(image_array / 255,decimals=round_decimal)
+        image_array = np.around(image_array / 255, decimals=round_decimal)
     return image_array
 
-def get_image_2_data_dict(image: NDArray) -> dict:
-    image_2_data_dict = {(i,j):np.asarray([i,j,image[i][j]])
-                         for i in range(image.shape[0])
-                         for j in range(image.shape[1])}
-    return image_2_data_dict
+
+def get_grid_2_data(image: NDArray) -> dict:
+    grid_2_data_dict = {(i, j): np.asarray([i, j, image[i][j]])
+                        for i in range(image.shape[0])
+                        for j in range(image.shape[1])}
+    return grid_2_data_dict
